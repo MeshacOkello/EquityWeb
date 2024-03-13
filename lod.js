ceed89984be4ad02bcb438bec7771f78c1da467b
MeshacOkello <145382061+MeshacOkello@users.noreply.github.com>
2024-03-13 08:52:16 +0300

    Add files via upload

---
+++ lod.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var loadingOverlay = document.querySelector('.center');
+    var content = document.querySelector('.content');
+
+    setTimeout(function () {
+        loadingOverlay.style.opacity = '0'; // Set opacity to 0 for smooth transition
+        loadingOverlay.style.visibility = 'hidden'; // Set visibility to hidden
+        content.style.opacity = '1'; // Set opacity to 1 for smooth transition
+        setTimeout(function () {
+            loadingOverlay.style.display = 'none';
+            content.style.display = 'block';
+        }, 2000); // Wait for the transition to complete (2 seconds)
+    }, 4000); // 4 seconds
+});
